718686ea34ad8b21ff320e56e8fc6e94908f6a49
Martijn Klene <martijn.klene@enreach.com>
2024-07-30 17:20:27 +0200

    Forgot one

--- src/main/kotlin/nl/martijnklene/hourreporting/microsoft/builder/GraphClientBuilder.kt
+++ src/main/kotlin/nl/martijnklene/hourreporting/microsoft/builder/GraphClientBuilder.kt
@@ -1,13 +1,16 @@
 package nl.martijnklene.hourreporting.microsoft.builder
 
 import com.microsoft.graph.serviceclient.GraphServiceClient
+import com.microsoft.kiota.authentication.BaseBearerTokenAuthenticationProvider
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.stereotype.Component
 
 @Component
 class GraphClientBuilder(private val provider: GraphAuthenticationProvider) {
     fun buildGraphClient(client: OAuth2AuthorizedClient): GraphServiceClient {
-        val item = provider.build(client)
-        return GraphServiceClient(item)
+        val accessTokenProvider = provider.build(client)
+        val provider = BaseBearerTokenAuthenticationProvider(accessTokenProvider)
+
+        return GraphServiceClient(provider)
     }
 }
