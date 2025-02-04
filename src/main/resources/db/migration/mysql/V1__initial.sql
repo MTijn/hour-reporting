3085a7e47e898eb3d4b9e9a6a0c93de1c7984958
Martijn Klene <martijn.klene@enreach.com>
2025-02-04 15:02:08 +0100

    Make the configuration available for secrets.

--- src/main/resources/db/migration/mysql/V1__initial.sql
+++ src/main/resources/db/migration/mysql/V1__initial.sql
@@ -1,24 +1,6 @@
-CREATE TABLE category
-(
-    user_id         uuid         NOT NULL,
-    category_name   varchar(255) NOT NULL,
-    jira_project_id uuid         NOT NULL,
-    jira_issue_key  varchar(255) NOT NULL,
-    "default"       bool DEFAULT NULL,
-    CONSTRAINT category_udx UNIQUE (user_id, category_name)
-);
-
-CREATE TABLE ignored_category
-(
-    user_id uuid         NOT NULL,
-    name    varchar(255) NOT NULL,
-    CONSTRAINT user_category_udx UNIQUE (user_id, name)
-);
-
 CREATE TABLE "user"
 (
     id   uuid NOT NULL,
     data json DEFAULT NULL,
     PRIMARY KEY (id)
 );
--- Dump completed on 2025-02-03 14:28:01
